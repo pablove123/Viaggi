69e7bb2bdeb9671e10d79913b9dcd9de29dbdb8a
Pablo Villedae <pablov.e12345@gmail.com>
2023-02-03 15:32:33 -0800

    add itinerary model'

---
+++ models/itinerary.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const itinerarySchema = new Schema({
+  experiences: [{type: Schema.Types.ObjectId, ref: "Experience"}],
+  name: String, 
+  City: String, 
+},{
+  timestamps: true,
+})
+
+const Itinerary = mongoose.model('Itinerary', itinerarySchema)
+
+export { Itinerary }
