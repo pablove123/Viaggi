3d5a43e63ccc297fefcae983ab3b014920614057
Pablo Villedae <pablov.e12345@gmail.com>
2023-02-03 15:28:01 -0800

    add experience Schema

---
+++ models/experience.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+  content: String, 
+  rating: Number, 
+})
+
+const expSchema = new Schema({
+  city: {type: String, enum:["Rome", "Venice", "Florence"]},
+  photo: String, 
+  name: String, 
+  category: String, 
+  description: String, 
+  review: [reviewSchema]
+},{
+  timestamps: true,
+})
+
+const Experience = mongoose.model('Experience', expSchema)
+
+export { Experience }
